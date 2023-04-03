b14a9cb5efc54b6ba6ac7b7af0b200206750d96e
Batwan01 <wldhks1120@naver.com>
2023-04-03 16:25:32 +0900

    good

---
+++ challenge/M2675.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	char S[20] = { 0, };
+	int T = 0, R = 0, i, j, k;
+
+	scanf("%d", &T);
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %s", &R, S);
+		for (j = 0; j < R; j++)
+		{
+			for (k = 0; k < R; k++)
+			{
+				printf("%c", S[j]);
+			}
+		}
+		printf("\n");
+	}
+}
