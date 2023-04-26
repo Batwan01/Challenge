4742b8f63fee9fc0daa6a2ba31f6a09038dc17a6
Batwan01 <wldhks1120@naver.com>
2023-04-27 01:41:45 +0900

    prac

---
+++ challenge/memo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+    double i = 1, sum=0, sum1=1;
+    for (; i < 65; i++) {
+        sum += i;
+        sum1 *= i;
+    }
+    printf("%f %f", sum, sum1);
+}
+
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
+//int main(void) {}
